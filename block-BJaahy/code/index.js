b232b3caac8cdc0dc083325c95a7fd4d26df8578
Arun <akr@7205gmail.com>
2022-09-22 00:15:21 +0530

    2nd commit

--- block-BJaahy/code/index.js
+++ block-BJaahy/code/index.js
@@ -10,16 +10,46 @@ class Square{
         return this.width * this.height;
     }
     set area(area){
-     this.width=Math.floor(Math.sqrt(area));
-     this.height=Math.floor(Math.sqrt(area));
+     this.width=Math.sqrt(area);
+     this.height=Math.sqrt(area);
 
     }
     get area(){
         return this.width * this.height;
     }
-    static isEqual(){
+    static isEqual(a,b){
+    return a.area === b.area;
      
     }
 }
 let s1 = new Square(6);
 let s2 = new Square(6);
+let s3 = new Square(12);
+
+class User{
+    constructor(firstName,lastName){
+        this.firstName=firstName;
+        this.lastName=lastName;
+    }
+    get fullName(){
+        return this.firstName+' '+this.lastName;
+    }
+    set fullName(Name){  
+        if(Name.length < 5){
+            alert(`Full name should be more than 5 characters`);
+        }else{
+            let fName=Name.split(' ')[0];
+            let lName=Name.split(' ')[1];
+            this.firstName=fName;
+            this.lastName=lName;
+        }
+        
+    }
+    nameContains(string){
+        return `${this.firstName} ${this.lastName}`.includes(string);
+    }
+}
+
+let user1= new User('Arya','Stark');
+let user2=new User('John','Snow');
+

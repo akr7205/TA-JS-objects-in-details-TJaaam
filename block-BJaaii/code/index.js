55eaeae80b7aa55a54f2c4b8e21fabefdb5317cb
Arun <akr@7205gmail.com>
2022-10-01 13:01:00 +0530

    1st commit

---
+++ block-BJaaii/code/index.js
@@ -0,0 +1,18 @@
+
+
+
+
+let myStack = new Stack();
+myStack.push('One');
+myStack.push('Two');
+console.log(myStack.length); // 2
+console.log(myStack.peek()); // "Two"
+console.log(myStack.peek(0)); // "One"
+console.log(myStack.reverse()); // ['Two', 'One']
+console.log(myStack.displayStack()); // 'Two One'
+myStack.pop();
+console.log(myStack.length); // 1
+console.log(myStack.peek()); // 'Two'
+console.log(myStack.isEmpty()); // false
+myStack.pop();
+console.log(myStack.isEmpty()); // true
